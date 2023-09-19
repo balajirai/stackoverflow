a50349a0cb468e6cecb1840bd9c51e9bd39233fd
balajirai <iam.balajirai@gmail.com>
2023-09-19 22:39:56 +0530

    body-parser added

--- server/index.js
+++ server/index.js
@@ -2,13 +2,12 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import ejs from "ejs";
+import bodyParser from "body-parser";
 
 import userRoutes from "./routes/users.js";
 import questionRoutes from "./routes/Questions.js";
 import answerRoutes from "./routes/Answers.js";
 import connectDB from "./connectMongoDb.js";
-
-// import users from "./models/auth.js";
 
 dotenv.config();
 connectDB();
@@ -16,6 +15,7 @@ connectDB();
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
